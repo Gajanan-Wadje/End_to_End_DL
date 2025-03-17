1b7572a570ff0b0189a3a1038474c1bdd64bc95d
Gajanan Wadje <katurava1@gmail.com>
2025-03-17 15:12:32 +0530

    Model Training Completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline #Stage 2
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline #Stage 3
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -10,4 +12,33 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
-        raise e                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+        raise e      
+
+
+#Stage 2
+        
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+ #Stage 3
+ 
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+#params_learning_rate: float #Added to resolve error
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -8,3 +9,30 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+#Stage 2
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+#Stage 3
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+    params_learning_rate: float # TO resolve error in stage 3
